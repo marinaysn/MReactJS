ecf36e6c0002df6ab43229873049f29e9d11c5ee
Marina <marinaysn@gmail.com>
2019-07-05 22:47:05 -0400

    adding first module code, props and state

--- reactcg/src/App.js
+++ reactcg/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [{name: 'Marina', age: 28},
+    {name: 'Pavel', age: 43},
+    {name: 'Alex', age: 15},
+    {name: 'Anna', age: 13}]
+  };
+
+  switchNameHandler = () => {
+    //console.log("This was clicked")
+    //DON'T DO THIS: this.state.persons[0].name = "Marina Ysn"
+    this.setState({persons: 
+      [{name: 'Marina Ysn', age: 43},
+      {name: 'Pavel', age: 43},
+      {name: 'Alex', age: 15},
+      {name: 'Anna', age: 13}]})
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, Marina</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1>Hi, Marina</h1>
+      <h3> React App</h3>
+
+      <p>This is really working</p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobby: Racing</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
       </div>
+
+      
     );
+   // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, Marina'), React.createElement('h3', null, 'React App'))
   }
 }
 
