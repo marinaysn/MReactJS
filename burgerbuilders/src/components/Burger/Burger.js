261c88e6dcd734977f9df25686893cd8e88368bb
Marina <marinaysn@gmail.com>
2019-07-21 12:11:50 -0400

    added 3 mathods to get items from BurgerBuilder to Burger and transfer them to array in order to work with data dynamically

--- burgerbuilders/src/components/Burger/Burger.js
+++ burgerbuilders/src/components/Burger/Burger.js
@@ -3,15 +3,48 @@ import cls from './burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+
+    // get items from BurgerBuilder to Burger and transfer them to array
+    // method 1
+    const locIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
+    // get items from BurgerBuilder to Burger and transfer them to array
+    // method 2
+    const ingredientsArr = this.props;
+    let locIngredients1 = [];
+    Object.keys(ingredientsArr).forEach(ingKey => {
+        for (let i = 0; i < ingredientsArr[ingKey]; i++)
+            locIngredients1.push(
+                <BurgerIngredient key={ingKey + i} type={ingKey} />)
+    });
+
+    // get items from BurgerBuilder to Burger and transfer them to array
+    // method 3
+    const ingredientsObj = props.ingredients;
+    const locIngredients2 = [];
+    for (const key in ingredientsObj) {
+        for (let i = 0; i < ingredientsObj[key]; i++) {
+            locIngredients2.push(<BurgerIngredient key={key + i} type={key} />)
+        }
+    }
+
+
+
+
     return (
         <div className={cls.Burger}>
             <BurgerIngredient type='BreadTop' />
-            
+
             <BurgerIngredient type='Meat' />
             <BurgerIngredient type='Cheese' />
             <BurgerIngredient type='Salad' />
             <BurgerIngredient type='Bacon' />
-            
+
             <BurgerIngredient type='BreadBottom' />
         </div>
     )
--- burgerbuilders/src/containers/BurderBuilder/BurderBuilder.js
+++ burgerbuilders/src/containers/BurderBuilder/BurderBuilder.js
@@ -5,10 +5,30 @@ import BurgerIngredient from '../../components/Burger/BurgerIngredient/BurgerIng
 
 
 export class BurderBuilder extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { ingredient: {
+    //         salad: 1,
+    //         bacon: 1,
+    //         cheese: 2,
+    //         meat: 2
+    //     }  };
+    //   }
+
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
             <Auxiliary>
-                <Burger />
+                <Burger ingredients = {this.state.ingredients} />
                 <BurgerIngredient />
             </Auxiliary>
         )
