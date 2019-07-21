fb7b273ac7921f019f48e8d05df29ec5c05888b4
Marina <marinaysn@gmail.com>
2019-07-20 20:14:23 -0400

    added basic component and container, added auxiliary file

--- burgerbuilders/src/App.js
+++ burgerbuilders/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
-
+import Layout from './components/Layout/Layout'
+import BurderBuilder from './containers/BurderBuilder/BurderBuilder'
 function App() {
   return (
     <div>
+    <Layout>
+      <BurderBuilder />
+    </Layout>
       
     </div>
   );
---
+++ burgerbuilders/src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import cls from './layout.module.css'
+import Auxiliary from '../../hoc/Auxiliary'
+
+
+
+const layout = (props) => {
+    return (
+        <Auxiliary>
+            <div>
+                Toolbar, SideDrawer, Backdrop
+            </div>
+
+            <main className={cls.Content}>
+                {props.children}
+            </main>
+        </Auxiliary>
+    )
+}
+
+export default layout
