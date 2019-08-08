5974cd64cadf2e5ed658b1d439e4d3cc13c472f4
Marina <marinaysn@gmail.com>
2019-08-08 18:03:50 -0400

    added services and title components, set them up and added icon blocks

--- resortsapp/src/pages/Rooms.js
+++ resortsapp/src/pages/Rooms.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import {Link} from 'react-router-dom';
+import Banner from "../components/Banner";
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
+        <Hero hero="roomsHero">
+            <Banner title="our rooms" subtitle="" >
+                <Link to="/rooms" className="btn-primary">
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
