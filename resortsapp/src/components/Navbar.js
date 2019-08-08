fb677561012a22d54625e47cd0a943a89f7ef8e1
Marina <marinaysn@gmail.com>
2019-08-07 23:16:10 -0400

    nav-bar

--- resortsapp/src/components/Navbar.js
+++ resortsapp/src/components/Navbar.js
@@ -8,20 +8,30 @@ export default class Navbar extends Component {
     state = {
         isOpen: false
     }
-    handleToggle = ()  => {
+    handleToggle = () => {
         this.setState({ isOpen: !this.state.isOpen });
     }
     render() {
         return (
             <nav className="navbar">
-             <div className="nav-center">
-                <div className="nav-header">
-                    <Link to="/" >
-                        <img src={logo} alt="Resor Main Page" /> 
-                    </Link>
-                 </div>
-             </div>
-                Hello from Navbar
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/" >
+                            <img src={logo} alt="Resort Main Page" />
+                        </Link>
+
+                        <button
+                            className="nav-btn"
+                            type="button"
+                            onClick={this.handleToggle}>
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                        <li> <Link to="/" /> Home </li>
+                        <li> <Link to="/rooms" /> Rooms </li>
+                    </ul>
+                </div>
             </nav>
         )
     }
