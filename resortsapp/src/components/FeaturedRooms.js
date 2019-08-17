38a13680881475448ce9f8281382ad360c2e87cd
Marina <marinaysn@gmail.com>
2019-08-17 11:17:26 -0400

    clean up files

--- resortsapp/src/components/FeaturedRooms.js
+++ resortsapp/src/components/FeaturedRooms.js
@@ -5,13 +5,15 @@ export default class FeaturedRooms extends Component {
 
     static contextType = RoomContext;
     render() {
-
+       
         // const {name, greeting} = this.context;
        const {featuredRooms : rooms} = this.context;
        console.log(rooms);
+       console.log("price " + rooms.name);
+      // console.log("price2 " + rooms[0].price);
         return (
             <div>
-                hello from Featured Rooms
+                hello from Featured Rooms {rooms.price}
             </div>
         )
     }
