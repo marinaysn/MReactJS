884622bd34e072f878a1584c9b2217282fe1ae03
Marina <marinaysn@gmail.com>
2019-08-26 23:34:40 -0400

    working on Filter

--- resortsapp/src/components/RoomsFilter.js
+++ resortsapp/src/components/RoomsFilter.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from "../context";
+import Title from "../components/Title";
+
+//get all unique values
+
+const getUnique = (items, value) => {
+
+    return [...new Set(items.map(item => item[value]))]
+}
 
 export default function RoomsFilter() {
+
+    const context = useContext(RoomContext);
+
+    const { rooms, handleChange, type, capacity, price, minPrice, maxPrice,
+        minSize, maxSize, breakfast, pets } = context;
+
+    //get unique types
+    let types = getUnique(rooms, 'type');
+        //add all
+    types = ['all', ...types];
+
+    //map to jsx
+        types = types.map( (i,idx)=>{
+            return <option value={i} key={idx}>{i}</option>
+        });
+
+
+
     return (
-        <div>
-            Hellow from Rooms Filter
-        </div>
+        <section className="filter-container">
+
+            <Title title="search-rooms" />
+
+            <form className="filter-form">
+                {/* Select Type*/}
+                <div className="form-group">
+                    <label htmlFor="type">Room Type</label>
+                    <select name="type" id="type" value={type} className="form-control"
+                        onChange={handleChange}>
+                        {types}
+
+                    </select>
+                </div>
+                {/* end of Select Type */}
+
+
+                {/*  */}
+                {/* end of  */}
+            </form>
+        </section>
     )
 }
 
--- resortsapp/src/context.js
+++ resortsapp/src/context.js
@@ -51,9 +51,14 @@ class RoomProvider extends Component {
     handleChange = event =>{
 
         const type = event.target.type;
-        const name = event.target.name;
-        const value = event.target.value;
         
+        const target = event.target;
+        const value = event.type === 'checkbox' ? target.checked: target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]:value
+        }, this.filterRooms)
+
         console.log(type);
         console.log(name);
         console.log(value);
